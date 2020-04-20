2325d48079d3e93051c7d63f6a38b3827d8e660a
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-04-21 00:54:17 +0530

    initial commit

---
+++ manage.py
@@ -0,0 +1,17 @@
+from flask import Flask,request,render_template
+import pandas as pd
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ model.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import pickle
+
+#import xgboost as xgb
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
+path ='iris.csv'
+
+
+colname = ['sepal_length','sepal_width','petal_length','petal_width','species']
+
+
+data = pd.read_csv(path,names=colname)
+
+data = data.values
+
+X = data[:,0:4]
+y = data[:,4]
+
+
+X_train,X_val,y_train,y_val = train_test_split(X,y,test_size=0.3,random_state=42)
+
+
+rf = RandomForestClassifier(random_state=123,n_estimators=100,max_depth = 4)
+rf.fit(X_train,y_train)
+
+#xgb_cl = xgb.XGBClassifier()
+
+rf_model= open('RFModel.pckl', 'wb')
+pickle.dump(rf, rf_model)
+rf_model.close()
