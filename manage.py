86b19bfcf05ee570dab37fca3457e666ae27a281
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-04-21 02:33:58 +0530

    version 1.2 logic changed

--- manage.py
+++ manage.py
@@ -21,7 +21,7 @@ def index():
         sepal_length = flask.request.form['sepal_length']
         sepal_width = flask.request.form['sepal_width']
         petal_length = flask.request.form['petal_length']
-        petal_width = flask.request.form['petal_length']
+        petal_width = flask.request.form['petal_width']
 
         # Make DataFrame for model
         input_variables = pd.DataFrame([[sepal_length,sepal_width, petal_length,petal_width]],
@@ -29,13 +29,13 @@ def index():
                                        dtype=float,
                                        index=['input'])
 
-        # Get the model's prediction
+        # Get the model's `prediction
         prediction = model.predict(input_variables)[0]
 
-        # Render the form again, but add in the prediction and remind user
+        # Render the form again, but add in the pred`iction and remind user
         # of the values they input before
         return flask.render_template('index.html',
-                                     original_input={'sepal_length':sepal_width,
+                                     original_input={'sepal_length':sepal_length,
                                                      'sepal_width':sepal_width,
                                                      'petal_length':petal_length,
                                                      'petal_width':petal_width},
