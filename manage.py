205198820b8e9859cb08242d4b93768bed940d47
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-04-21 01:34:26 +0530

    version_1 complete

--- manage.py
+++ manage.py
@@ -1,18 +1,46 @@
-from flask import Flask,request,render_template
+import flask
+import pickle
 import pandas as pd
 
+# Use pickle to load in the pre-trained model
+with open(f'RFModel.pckl', 'rb') as f:
+    model = pickle.load(f)
 
-app = Flask(__name__)
+# Initialise the Flask app
+app = flask.Flask(__name__, template_folder='templates')
 
-
-@app.route('/')
+# Set up the main route
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if flask.request.method == 'GET':
+        # Just render the initial form, to get input
+        return(flask.render_template('index.html'))
 
+    if flask.request.method == 'POST':
+        # Extract the input
+        sepal_length = flask.request.form['sepal_length']
+        sepal_width = flask.request.form['sepal_width']
+        petal_length = flask.request.form['petal_length']
+        petal_width = flask.request.form['petal_length']
 
+        # Make DataFrame for model
+        input_variables = pd.DataFrame([[sepal_length,sepal_width, petal_length,petal_width]],
+                                       columns=['sepal_length', 'sepal_width', 'petal_length','petal_width'],
+                                       dtype=float,
+                                       index=['input'])
 
+        # Get the model's prediction
+        prediction = model.predict(input_variables)[0]
 
+        # Render the form again, but add in the prediction and remind user
+        # of the values they input before
+        return flask.render_template('index.html',
+                                     original_input={'sepal_length':sepal_width,
+                                                     'sepal_width':sepal_width,
+                                                     'petal_length':petal_length,
+                                                     'petal_width':petal_width},
+                                     result=prediction,
+                                     )
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
